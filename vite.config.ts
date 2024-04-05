8e18bdf180ef2cfda51f3132d0f54a55c6ccb399
kyoung-jnn <55469709+kyoung-jnn@users.noreply.github.com>
2024-04-06 02:49:00 +0900

    feat: initial emotion

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import babel from "vite-plugin-babel";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    babel({
+      babelConfig: {
+        plugins: ["@emotion/babel-plugin"],
+      },
+    }),
+    react({ jsxImportSource: "@emotion/react" }),
+  ],
+});
