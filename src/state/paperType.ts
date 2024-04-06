13856840066fb656a1084fcf3ae6ef98cf2d4816
kyoung-jnn <55469709+kyoung-jnn@users.noreply.github.com>
2024-04-06 19:04:34 +0900

    feat: 퍼널 UI 제작

--- src/state/paperType.ts
+++ src/state/paperType.ts
@@ -1,5 +1,5 @@
-import { atom } from 'jotai';
+import { atom } from 'jotai'
 
-export type PaperType = "모눈종이" | "줄노트";
+export type PaperType = '모눈종이' | '줄노트'
 
-export const paperTypeState = atom<PaperType | null>(null);
+export const paperTypeState = atom<PaperType | null>(null)
