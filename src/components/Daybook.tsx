e798e4faf1e69f24dec6cd94636a56c3bf85e28d
kyoung-jnn <55469709+kyoung-jnn@users.noreply.github.com>
2024-04-06 10:37:52 +0900

    feat: image hook 및 레이아웃 설정 추가

--- src/components/Daybook.tsx
+++ src/components/Daybook.tsx
@@ -1,19 +1,28 @@
-import styled from "@emotion/styled";
 import { colors } from "../theme";
 import getFontStyle from "../theme/font/getFontSize";
+import { ForwardedRef, forwardRef } from "react";
+import { css } from "@emotion/react";
 
-function Daybook() {
-  return <Wrapper>일지~</Wrapper>;
+interface Props {}
+
+// eslint-disable-next-line no-empty-pattern
+function Daybook({}: Props, ref: ForwardedRef<HTMLDivElement>) {
+  return (
+    <div
+      ref={ref}
+      css={css`
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        height: 100px;
+        border: 1px solid ${colors.primaryLight};
+        ${getFontStyle("header1")};
+      `}
+    >
+      일지~
+    </div>
+  );
 }
 
-export default Daybook;
-
-const Wrapper = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  height: 100px;
-  border: 1px solid ${colors.primaryLight};
-  ${getFontStyle("header1")};
-`;
+export default forwardRef(Daybook);
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 import Router from "./Router";
+import Layout from "./components/Layout";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Router />
+    <Layout>
+      <Router />
+    </Layout>
   </React.StrictMode>
 );
