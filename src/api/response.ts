a39368e52504495973b17d0c080bdf2b7f8782f8
kyoung-jnn <55469709+kyoung-jnn@users.noreply.github.com>
2024-04-06 20:27:26 +0900

    chore: 일지 작성 API 연결

---
+++ src/api/response.ts
@@ -0,0 +1,9 @@
+export type PostDaybookResponse = {
+  boardId: number;
+  content: string;
+  authorId: number;
+  category: string;
+  hearts: number;
+  respectBoardId: number;
+  hashtags: string;
+};
