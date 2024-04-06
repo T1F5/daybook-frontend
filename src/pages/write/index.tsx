562caa737e78dc6963c243f3ded0fbf5413bad38
kyoung-jnn <55469709+kyoung-jnn@users.noreply.github.com>
2024-04-06 13:33:14 +0900

    chore: css 추가

--- src/pages/write/index.tsx
+++ src/pages/write/index.tsx
@@ -1,47 +1,48 @@
-import Header from "@components/Header";
-import Button from "@components/common/Button";
-import { css } from "@emotion/react";
-import styled from "@emotion/styled";
-import useStep, { STEP, StepContent, StepContentMap } from "@hooks/useStep";
-import getFontStyle from "@theme/font/getFontSize";
-import 쓰기 from "@pages/write/쓰기";
-import 완료 from "@pages/write/완료";
-import 탄생 from "@pages/write/탄생";
-import 형태 from "@pages/write/형태";
+import Header from '@components/Header'
+import Button from '@components/common/Button'
+import { css } from '@emotion/react'
+import styled from '@emotion/styled'
+import useStep, { STEP, StepContent, StepContentMap } from '@hooks/useStep'
+import getFontStyle from '@theme/font/getFontSize'
+import 쓰기 from '@pages/write/쓰기'
+import 완료 from '@pages/write/완료'
+import 탄생 from '@pages/write/탄생'
+import 형태 from '@pages/write/형태'
 
 const STEP_COMPONENTS = {
   [STEP.쓰기]: 쓰기,
   [STEP.완료]: 완료,
   [STEP.탄생]: 탄생,
   [STEP.형태]: 형태,
-};
-
+}
 function Write() {
-  const { step, nextStep, previousStep } = useStep();
+  const { step, nextStep, previousStep } = useStep()
 
   const { title, description, buttonText } = StepContentMap.get(
     step
-  ) as StepContent;
+  ) as StepContent
 
   return (
     <>
       <Header title="일지 작성하기">
-        {step !== "완료" && (
+        {step !== '완료' && (
           <Header.Button variety="back" onClick={previousStep} />
         )}
       </Header>
       <Wrapper>
         <p
           css={css`
-            ${getFontStyle("header1")}
+            white-space: pre-wrap;
+            ${getFontStyle('header1')}
           `}
         >
           {title}
         </p>
         <p
           css={css`
-            ${getFontStyle("title4")}
+            white-space: pre-wrap;
             margin-top: 9px;
+            ${getFontStyle('title4')}
           `}
         >
           {description}
@@ -52,14 +53,14 @@ function Write() {
         <Button onClick={nextStep}>{buttonText}</Button>
       </FloatingButtonWrapper>
     </>
-  );
+  )
 }
 
-export default Write;
+export default Write
 
 const Wrapper = styled.div`
   padding: 24px 20px;
-`;
+`
 
 const FloatingButtonWrapper = styled.div`
   position: fixed;
@@ -68,4 +69,4 @@ const FloatingButtonWrapper = styled.div`
   left: 0;
   padding: 8px 20px 16px;
   box-sizing: border-box;
-`;
+`
