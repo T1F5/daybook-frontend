5a3107f9ead85dc82aa9ae971320d5942cc926cf
kyoung-jnn <55469709+kyoung-jnn@users.noreply.github.com>
2024-04-07 09:51:20 +0900

    chore: button

--- src/components/Detail.tsx
+++ src/components/Detail.tsx
@@ -4,35 +4,33 @@ import getFontStyle from "@theme/font/getFontSize";
 import { Link } from "react-router-dom";
 
 interface DetailProps {
-    daybookId: number;
+  daybookId: number;
 }
 
 const Detail = ({ daybookId }: DetailProps) => {
-    return (
-        <Wrapper>
-            <Link to={`/detail/${daybookId}`}>
-                자세히 보기
-            </Link>
-        </Wrapper>
-    );
+  return (
+    <Link to={`/detail/${daybookId}`}>
+      <Wrapper>자세히 보기</Wrapper>
+    </Link>
+  );
 };
 
 export default Detail;
 
 const Wrapper = styled.section`
-    width: 100%;
-    height: 64px;
+  width: 100%;
+  height: 64px;
 
-    background-color: ${colors.primaryLight};
-    ${getFontStyle("header4")};
+  background-color: ${colors.primaryLight};
+  ${getFontStyle("header4")};
+  color: white;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  a {
+    text-decoration: none;
     color: white;
-
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    a {
-        text-decoration: none;
-        color: white;
-    }
-`
+  }
+`;
